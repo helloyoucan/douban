8de736870b56cbe1ead337777d1683d94980d088
helloyoucan <helloyoucan@163.com>
2017-09-05 09:19:51 +0800

    添加 vconsole

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import Main from './components/main/main.js';
 import './scss/common.scss';
-const App = function(){
-	var app = document.querySelector("#app");
-	var main = new Main();
-	app.innerHTML = main.tpl({});
+//import './vconsole.min'
+const App = function () {
+    var app = document.querySelector("#app");
+    var main = new Main();
+    app.innerHTML = main.tpl({});
     main.init();
 }
 new App();
