23da69bc03afd9913529bcfd43352aabf291de40
helloyoucan <helloyoucan@163.com>
2017-09-10 22:14:00 +0800

    修改请求的url

--- api/api.js
+++ api/api.js
@@ -1,8 +1,25 @@
+/*export default {
+    movie: {
+        hot: '/apis/v2/movie/in_theaters',
+        now: '/apis/v2/movie/in_theaters',
+        top: '/apis/v2/movie/in_theaters',
+        search: '/apis/v2/movie/search'
+    }
+}*/
 export default {
     movie: {
-        hot: '/douban/movie/in_theaters',
-        now: '/douban/movie/in_theaters',
-        top: '/douban/movie/in_theaters',
-        search: '/douban/movie/search'
+        hot: 'http://123.207.77.245:8081/apis/v2/movie/in_theaters',
+        now: 'http://123.207.77.245:8081/apis/v2/movie/coming_soon',
+        top: 'http://123.207.77.245:8081/apis/v2/movie/top250',
+        search: 'http://123.207.77.245:8081/apis/v2/movie/search'
     }
 }
+/*export default {
+    movie: {
+        hot: 'http://localhost:8081/apis/v2/movie/in_theaters',
+        now: 'http://localhost:8081/apis/v2/movie/in_theaters',
+        top: 'http://localhost:8081/apis/v2/movie/in_theaters',
+        search: 'http://localhost:8081/apis/v2/movie/search'
+    }
+}*/
+//123.207.77.245
